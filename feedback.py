a8043ed289c492ca5a7544c09999f5ba38ee4236
LFGaming <38732192+LFGaming@users.noreply.github.com>
2022-09-27 15:30:52 +0200

    Update feedback.py

--- feedback.py
+++ feedback.py
@@ -2,19 +2,22 @@
 name = str(input("Wat is je naam? (optioneel) "))
 feedback = str(input("Wat was je ervaring met het station? "))
 
-if name == "":
-    print("Anoniem")
-else:
-    print(f"Naam: {name}")
+def response(name, feedback):
+    if name == "":
+        print("Anoniem")
+    else:
+        print(f"Naam: {name}")
 
-if len(feedback) >= 140:
-    print("Te veel letters")
-else:
-    print(f"Gaf als feedback: {feedback}")
+    if len(feedback) >= 140:
+        print("Te veel letters")
+    else:
+        print(f"Gaf als feedback: {feedback}")
 
-with open('scheldwoorden.txt') as f:
-    if feedback in f.read():
-        print("Er zit een scheldwoord in de feedback")
+    with open('scheldwoorden.txt') as f:
+        if feedback in f.read():
+            print("Er zit een scheldwoord in de feedback")
+
+r = response(name, feedback)
 
 # scheldwoorden komen van https://www.dutchmultimedia.nl/scheldwoordenboek-1-000-den-nederlandse-scheldwoorden/
 # %%
