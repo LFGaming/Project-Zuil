e3984aea345660fe788552d14d5ba670432a48f6
LFGaming <38732192+LFGaming@users.noreply.github.com>
2022-10-03 16:04:30 +0200

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -2,7 +2,11 @@ keuzen = str(input("Is deze feedback goed gekeurd? "))
 
 def moderation():
     with open('feedback_check.txt', 'r') as f:
-        print(f"{f}")
+        z = f.readlines()
+        for n in z[2:]:
+            print(n.strip())
         if keuzen == "goed":
             with open('feedbacks.txt', 'a') as t:
                 t.write(f"")
+
+r = moderation()
