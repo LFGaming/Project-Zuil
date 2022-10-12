469f90efc4398296d84c2daee2809bd756c404b1
LFGaming <38732192+LFGaming@users.noreply.github.com>
2022-10-12 20:32:35 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from datetime import date
 
+vandaag = date.today()
 
-def show_entry_fields():
+def response():
     print(f"First Name: {input1.get()} \nLast Name: {input2.get()}")
     if input1.get() == "":
         print("Anoniem")
@@ -19,38 +21,11 @@ def show_entry_fields():
                 "Er zit een scheldwoord in de feedback of de naam, het wordt naar de check gestuurd")
             with open('feedback_check.txt', 'a') as g:
                 g.write(
-                    f"Naam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
+                    f"{vandaag}\nNaam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
         else:
             with open('feedbacks.txt', 'a') as t:
                 t.write(
-                    f"Naam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
-
-# name = str(input("Wat is je naam? (optioneel) "))
-# feedback = str(input("Wat was je ervaring met het station? "))
-
-
-'''def response(name, feedback):
-    if name == "":
-        print("Anoniem")
-    else:
-        print(f"Naam: {name.get()}")
-
-    if len(feedback.get()) >= 140:
-        print("Te veel letters")
-    else:
-        print(f"Gaf als feedback: {feedback.get()}")
-
-    with open('scheldwoorden.txt', 'r') as f:
-        if feedback.get() in f.read():
-            print(
-                "Er zit een scheldwoord in de feedback of de naam, het wordt naar de check gestuurd")
-            with open('feedback_check.txt', 'a') as g:
-                g.write(
-                    f"Naam: {name.get()} \nFeedback: {feedback.get()}\n \n --------------------------- \n")
-        else:
-            with open('feedbacks.txt', 'a') as t:
-                t.write(
-                    f"Naam: {name.get()} \nFeedback: {feedback.get()}\n \n --------------------------- \n")'''
+                    f"{vandaag}\nNaam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
 
 
 master = tk.Tk()
@@ -72,16 +47,10 @@ tk.Button(master,
                                     sticky=tk.W,
                                     pady=4)
 tk.Button(master,
-          text='Show', command=show_entry_fields).grid(row=3,
+          text='Show', command=response).grid(row=3,
                                                        column=1,
                                                        sticky=tk.W,
                                                        pady=4)
-'''tk.Button(master,
-          text='test', command=response(input1, input2)).grid(row=4,
-                                                       column=1,
-                                                       sticky=tk.W,
-                                                       pady=4)'''
-
 
 tk.mainloop()
 
