66fbb5c56b2677f3b09eb87800c8815bd491e084
LFGaming <38732192+LFGaming@users.noreply.github.com>
2022-11-03 16:19:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,88 +3,81 @@ import datetime
 import random
 import psycopg2
 
-#id = input("Unique id: ")
-#name = input("Name: ")
-#bericht = input("Bericht: ")
-#tijd = input("Tijd: ")
-
-
 vandaag = datetime.datetime.today()
 
 with open("stations.txt","r") as file:
     lines = file.read().splitlines()
     station = random.choice(lines)
 
+name = str(input("Wat is je naam? (optioneel) "))
+feedback = str(input("Wat was je ervaring met het station? "))
+
+
+#name = input("Naam: ")
+#bericht = input("Bericht: ")
+stationnaam = str(station)
+tijd = str(vandaag)
+
 def response():
-    print(f"First Name: {input1.get()} \n Feedback: {input2.get()}")
-    if input1.get() == "":
+    print(f"First Name: {name} \n Feedback: {feedback}")
+    if name == "":
         print("Anoniem")
     else:
-        print(f"Naam: {input1.get()}")
+        print(f"Naam: {name}")
 
-    if len(input2.get()) >= 140:
+    if len(feedback) >= 140:
         print("Te veel letters")
     else:
-        print(f"Gaf als feedback: {input2.get()}")
+        print(f"Gaf als feedback: {feedback}")
 
     with open('scheldwoorden_kort.txt', 'r') as f:
-        if input2.get() in f.read():
+        if feedback in f.read():
             print(
-                "Er zit een scheldwoord in de feedback of de naam, het wordt naar de check gestuurd")
-            with open('feedback_check.txt', 'a') as g:
-                g.write(
-                    f"{vandaag}\nBij station {station}\nNaam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
+                "Er zit een scheldwoord in de feedback of de naam, het wordt afgekeurt.")
         else:
-            with open('feedbacks.txt', 'a') as t:
-                t.write(
-                    f"{vandaag}\nBij station {station}\nNaam: {input1.get()} \nFeedback: {input2.get()}\n \n --------------------------- \n")
+            # put message in database
+
+            connection_string = "host='localhost' dbname='proja' user='postgres' password='PW'"
+
+            conn = psycopg2.connect(connection_string) 
+            cursor = conn.cursor()
+
+            query = """INSERT INTO bericht (naam, bericht, stationnaam, tijd)
+                    VALUES (%s, %s, %s, %s);"""      # Always use %s as a placeholder. Pyscopg will
+                                                    # convert the datatype and add quotes if necessary!
+            data = (name, feedback, stationnaam, tijd)
+            cursor.execute(query, data)             # The second parameter must be list or tuple
+            conn.commit()
+            conn.close()
+
+            #with open('feedbacks.txt', 'a') as t:
+            #    t.write(
+            #        f"{vandaag}\nBij station {station}\nNaam: {name} \nFeedback: {feedback}\n \n --------------------------- \n")
 
 
-master = tk.Tk()
-tk.Label(master,
-         text="First Name").grid(row=0)
-tk.Label(master,
-         text="Feedback").grid(row=1)
+r = response()
 
-input1 = tk.Entry(master)
-input2 = tk.Entry(master)
+# get message from database
+def read():
+    connection_string = "host='localhost' dbname='proja' user='postgres' password='PW'"
 
-input1.grid(row=0, column=1)
-input2.grid(row=1, column=1)
+    conn = psycopg2.connect(connection_string) 
+    cursor = conn.cursor()
 
-tk.Button(master,
-          text='Quit',
-          command=master.quit).grid(row=3,
-                                    column=0,
-                                    sticky=tk.W,
-                                    pady=4)
-tk.Button(master,
-          text='Show', command=response).grid(row=3,
-                                                       column=1,
-                                                       sticky=tk.W,
-                                                       pady=4)
+    query = "select * from bericht"      # Always use %s as a placeholder. Pyscopg will
+                                            # convert the datatype and add quotes if necessary!
+    cursor.execute(query)
+    print("Selecting rows from mobile table using cursor.fetchall")
+    mobile_records = cursor.fetchall()
 
-tk.mainloop()
+    print("Print each row and it's columns values")
+    for row in mobile_records:
+        print("Naam = ", row[0])
+        print("Bericht  = ", row[1])
+        print("Tijd = ", row[2])
+        print("Modnummer = ", row[3])
+        print("Stationnaam = ", row[4], "\n")
+    conn.commit()
+    conn.close()
 
-#r = response(input1, input2)
-
-# Database
-
-id = input("Unique id: ")
-name = input1.get()
-bericht = input2.get()
-location = station
-date = vandaag
-
-connection_string = "host='localhost' dbname='proja' user='postgres' password='PW'"
-
-conn = psycopg2.connect(connection_string) 
-cursor = conn.cursor()
-
-query = """INSERT INTO bericht (idnummer, naam, bericht, date, )
-           VALUES (%s, %s, %s);"""      # Always use %s as a placeholder. Pyscopg will
-                                        # convert the datatype and add quotes if necessary!
-data = (id, name, bericht)#, tijd)
-cursor.execute(query, data)             # The second parameter must be list or tuple
-conn.commit()
-conn.close()
+re = read()
