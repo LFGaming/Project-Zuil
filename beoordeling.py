c10552c57121e8063979adbf825c461f3e4cfa16
LFGaming <38732192+LFGaming@users.noreply.github.com>
2022-11-04 16:58:31 +0100

    Update beoordeling.py

--- beoordeling.py
+++ beoordeling.py
@@ -1,18 +1,44 @@
 import psycopg2
 
-def read():
-    connection_string = "host='localhost' dbname='proja' user='postgres' password='PW'"
+def inloggen():
+    
+    name = str(input("Naam: "))
+
+    # zoek of deze naam voorkomt in de database:
+    connection_string = "host='localhost' dbname='proja' user='postgres' password='Postgresqlekul!1'"
 
     conn = psycopg2.connect(connection_string) 
     cursor = conn.cursor()
 
-    query = "select * from bericht"      # Always use %s as a placeholder. Pyscopg will
+    query = "select * from moderator where naam = %s"      # Always use %s as a placeholder. Pyscopg will
+                                            # convert the datatype and add quotes if necessary!
+    arg = (name, )
+    cursor.execute(query, arg)
+    records = cursor.fetchone()
+    if len(records) == 0:
+        print("U bent nog niet geregistreerd als moderator. Gebruik addmod.py")
+        return -1
+    else:
+        print(f"Welkom {name} met email: {records[2]}")
+        modnum = records[0]
+        return modnum
+
+
+def beoordeel(mod):
+    connection_string = "host='localhost' dbname='proja' user='postgres' password='Postgresqlekul!1'"
+
+    conn = psycopg2.connect(connection_string) 
+    cursor = conn.cursor()
+
+    query = "select * from bericht where beoordeling is null"      # Always use %s as a placeholder. Pyscopg will
                                             # convert the datatype and add quotes if necessary!
     cursor.execute(query)
-    print("Selecting rows from mobile table using cursor.fetchall")
     records = cursor.fetchall()
+    if len(records) == 0:
+        print("Geen nieuwe berichten")
+    else:
+        print("Te beoordelen berichten: ")
 
-    print("Print each row and it's columns values")
     for row in records:
         print("IDnummer = ", row[0])
         print("Naam = ", row[1])
@@ -20,7 +46,24 @@ def read():
         print("Tijd = ", row[3])
         print("Modnummer = ", row[4])
         print("Stationnaam = ", row[5], "\n")
+        keuze = input("Goed of fout? ")
+        if keuze == 'g':
+            
+            
+            query = """update bericht set beoordeling = 'goed', modnummer = %s where idnummer = %s"""
+            arg = (mod, str(row[0]))
+            print(type(arg))
+            cursor.execute(query, arg)             # The second parameter must be list or tuple
+            
+        else:
+
+            query = """update bericht set beoordeling = 'fout', modnummer = %s where idnummer = %s"""
+            arg = (mod, str(row[0]))
+            cursor.execute(query, arg)             # The second parameter must be list or tuple
+
     conn.commit()
     conn.close()
 
-re = read()
+mod = inloggen()
+if mod != -1:
+    beoordeel(mod)
